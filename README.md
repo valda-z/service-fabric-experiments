bf122745c1e99a42c53618ca267249c2291ad976
valda-z <valdemar.zavadsky@microsoft.com>
2017-10-08 20:34:27 +0200

    Secured SF cluster tested and working now

--- README.md
+++ README.md
@@ -33,5 +33,50 @@ Before publish template (steps needed for Point To Site VPN):
     * https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-howto-point-to-site-resource-manager-portal  
  
 
+### SFClusterVMSSMonitoringAppGWSecured
 
+Secured SF cluster with internal load balancer, application gateway which exposes public IP address and traffic to applications (template exposes 3 application ports from SF cluster). 
+
+**Cluster is secured by generated certificates and AD, please follow steps in this article (generate keys, KeyVault and Active Directory app)** : 
+https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-cluster-creation-via-arm
+* user can login to cluster only if has access right to tenant and application itself 
+* cluster is secured by certificates
+* for encrypting of secrets we can use different certificates than used for cluster security itself
+
+
+
+Service Fabric cluster is provisioned with enabled addon "RepairManager" which provide to cluster automation OS updates. See: https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-patch-orchestration-application 
+
+Traditionally template creates Application Insight and OMS workspace, all nodes are connected to OMS workspace. Application Insights and OMS is used for SF Cluster monitoring.
+For SF cluster management and deployment is used VPN Gateway in Point To Site mode.
+
+![img1.png](img/img1.png)
+
+#### Before you will provision your cluster
+
+##### Steps needed for SF cluster certificates and AD authorization
+* get AAD Tenant ID for your Active Directory (In Azure portal select "Azure Active Directory", in selected AAD go to "Properties" -> there you can see "Direcotry ID" which is Tennat ID.
+* edit script `deploy.ps1` and define your values in script header which reflects your environment - you can find there two sections with label `### DEFINE YOUR OWN VALUES THERE
+* run script `deploy.ps1` (don't forget to change your working directory to the directory with deploy.ps1 script (it means to directory `SFClusterVMSSMonitoringAppGWSECURED`.
+* finally you have to grant access to AAD users for Service Fabric Cluster
+ * In Azure portal go to your active directory    
+ * Select there "Enterprise application option in list of actions
+ * ![img2.png](img/img2.png "")
+ * Than go to list of all applications and change filter to "All Applications" and "Any" and hit button Apply
+ * ![img2.png](img/img3.png "")
+ * From the application list select you Service Fabric cluster application - there is something like `<YOUR_SF_CLUSTER_NAME>_Cluster`
+ * There you can use option "Users and Groups" to grant access to users or groups from your AAD (grant  user access to "Admin" role.
+ * ![img2.png](img/img4.png "")
+
+##### Steps needed for Point To Site VPN:
+* prepare ROOT and Client certificates for Point To Site VPN
+    * https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-certificates-point-to-site
+    * https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-certificates-point-to-site#clientcert
+* Export public part of ROOT key to CER file and use it in template parameters
+* Setup VPN client on client machine
+    * https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-howto-point-to-site-resource-manager-portal  
+ 
+##### Future reading and credits to
+
+This repository (security part is inspired by **JJ's** github for secure Service Fabric cluster: https://github.com/jjindrich/jjazure-servicefabric . Also there you can find future reading about **encrypted properties/variables for development and production Service Fabric cluster** and how to handle these security assets.
 
---
+++ SFClusterVMSSMonitoringAppGWSECURED/deploy.ps1
@@ -0,0 +1,98 @@
+﻿# variables
+### DEFINE YOUR OWN VALUES THERE
+$RGName="SF"
+$KeyVaultName="valdaSFKeyVault"
+$ResourceGroupLocation="South Central US"
+$dnsName="10.10.0.4"
+$clusterName="valdasf"
+$aadTenantId="00000000-0000-0000-0000-000000000000"
+$certImportName="sfCert"
+$certImportNameApp="sfCertApp"
+
+Write-Output "#################### Starting script"
+
+# Login to Azure
+Login-AzureRmAccount
+
+# Select your subscription if necessary
+#Get-AzureRmSubscription
+#Set-AzureRmContext -SubscriptionId <guid>
+
+# create resource group
+Write-Output ">>>>>> Creating Resource group"
+$rg=New-AzureRmResourceGroup -Name $RGName -Location $ResourceGroupLocation
+
+# setup key vault (unique name)
+Write-Output ">>>>>> Creating Key vault"
+$keyvault=New-AzureRmKeyVault -VaultName $KeyVaultName -ResourceGroupName $RGName -Location $ResourceGroupLocation -EnabledForDeployment
+
+# Create new certificate
+Write-Output ">>>>>> Generating certificates"
+Import-Module .\New-SelfSignedCertificateEx.ps1
+$securePassword = ConvertTo-SecureString -String "azure" -AsPlainText -Force
+
+$cert=New-SelfsignedCertificateEx -Subject "CN=$($dnsName)" -EKU "Server Authentication", "Client authentication", "Document Encryption" -KeyUsage "KeyEncipherment, DigitalSignature, DataEncipherment" -Path "mycertkey.pfx" -Password $securePassword -Exportable
+$certapp=New-SelfsignedCertificateEx -Subject "CN=$($dnsName)" -EKU "Server Authentication", "Client authentication", "Document Encryption" -KeyUsage "KeyEncipherment, DigitalSignature, DataEncipherment" -Path "mycertkeyapp.pfx" -Password $securePassword -Exportable
+
+# import certificate to keyvalut
+Write-Output ">>>>>> Importing certificates to keyvault"
+Set-ExecutionPolicy Unrestricted -Scope Process
+
+$loadedCert=Import-AzureKeyVaultCertificate -VaultName $KeyVaultName -Name $certImportName -FilePath "mycertkey.pfx" -Password $securePassword
+$loadedCertApp=Import-AzureKeyVaultCertificate -VaultName $KeyVaultName -Name $certImportName -FilePath "mycertkeyapp.pfx" -Password $securePassword
+
+# AAD staff
+Write-Output ">>>>>> Create AAD application"
+$aad=.\AADTool\SetupApplications.ps1 -TenantId $aadTenantId -ClusterName $clusterName -WebApplicationReplyUrl "https://$($dnsName):19080/Explorer/index.html"
+
+# output
+
+Write-Output "#################### OUTPUT values #######################"
+Write-Output "##### AAD staff"
+Write-Output ">>> aadTenantId: $($aad.TenantId)"
+Write-Output ">>> aadClusterApplicationId: $($aad.WebAppId)"
+Write-Output ">>> aadClientApplicationId: $($aad.NativeClientAppId)"
+
+Write-Output "##### Certificate staff"
+Write-Output ">>> clusterCertificateThumbprint: $($loadedCert.Certificate.Thumbprint)"
+Write-Output ">>> clusterCertificateUrlValue: $($loadedCert.SecretId)"
+Write-Output ">>> applicationCertificateUrlValue: $($loadedCertApp.SecretId)"
+Write-Output ">>> sourceVaultvalue: $($keyvault.ResourceId)"
+
+Write-Output "#################### OUTPUT assets #######################"
+Write-Output "##### please collect files mycertkey.pfx (cluster certificate) and mycertkeyapp.pfx (application certificate)."
+
+# provision template to Azure subscription
+Write-Output ">>>>>> Provisioning of template ..."
+
+### DEFINE YOUR OWN VALUES THERE
+$secureClusterPassword = ConvertTo-SecureString -String "pwd123...pwd" -AsPlainText -Force
+New-AzureRmResourceGroupDeployment -Name SFDeployment -ResourceGroupName $RGName `
+  -TemplateFile azuredeploy.json `
+  -clusterName $clusterName `
+  -computeLocation $ResourceGroupLocation `
+  -adminUserName "azureadmin" `
+  -adminPassword $secureClusterPassword `
+  -omsWorkspacename "valdasfoms" `
+  -applicationInsightsName "valdasfappins" `
+  -omsRegion "East US" `
+  -appInsightsRegion "East US" `
+  -clusterCertificateThumbprint $loadedCert.Certificate.Thumbprint `
+  -clusterCertificateUrlValue $loadedCert.SecretId `
+  -applicationCertificateUrlValue $loadedCertApp.SecretId `
+  -sourceVaultvalue $keyvault.ResourceId `
+  -aadTenantId $aad.TenantId `
+  -aadClusterApplicationId $aad.WebAppId `
+  -aadClientApplicationId $aad.NativeClientAppId `
+  -clientRootCertName "RootCert1" `
+  -clientRootCertData "MIIC5zCCAc+gAwIBAgIQE6mqUPntLbhI7xYn8FWGKzANBgkqhkiG9w0BAQsFADAWMRQwEgYDVQQDDAtQMlNSb290Q2VydDAeFw0xNzA5MTIwNjI3MzNaFw0xODA5MTIwNjQ3MzNaMBYxFDASBgNVBAMMC1AyU1Jvb3RDZXJ0MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1gBHkxhaJJnNyA3Gfku91Mk3hndXsgzRwIsHT3zMCiR1kvYmKD5mUkmAYieCnEstpuBm+5F0Z07yMWegc90umMZ5ih4AN2zEWtfteuHcVcrrvPWrodRPcVjVOEG3t1sWmd5wnedzqH+nTi8GWqDTpwyU+M8X7VMFbyfrDWP7VCgNyzYFUPrBSy3Q+Eg5iHZl4tieO4Lu8yTvtZNdesCMm1nwNJroL+04LA+umalNo0B7q6sKOAlUaJwTq2brAKOEaJJvmZff8r8QFwG+L6LoPXmBSxrBnmK5ZMOFSzw/XbpREQAeA4qzrQFXJfxJeXTbyRtDHCEC7WWEmJnBI6hxJQIDAQABozEwLzAOBgNVHQ8BAf8EBAMCAgQwHQYDVR0OBBYEFG5Vf1SQHH/jkaH9XSJeohKlAwITMA0GCSqGSIb3DQEBCwUAA4IBAQB+NZhdvuBCcBrvdLbHcZ9UjVLwRqHVVXTIgCx3jD5C4Vwo/dOfRsGC0xouAtZbOwTk4cOmjHZ74gW+I52Ply2Evh2ULeuJtwdltTU3VlRJKWMoCtk2ZzcmU2C0kwXtBPAgYYxmMN5qQDojxLy6U20XdY+HsZkzvtESsjiYV6k8hg3mxpE2mqFaQvAVbSjno/vnGjoY/znm5HgAoT+opcp5fwUWq3RlwtM8N/S9O2RnLsDng7OPE3NMkdm971DkWYOHAsrvrisf/P9MNwgXcWSt7GjAmXEz8Lj7FVsUUSgl2KTQn+J9zRfQb5RpbZhXZaajrRx4XWgyby+qpoYhPKcL" 
+
+
+
+
+
+
+
+
+
+
